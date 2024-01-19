3b4a7e4c48a552bde63ebda523aa79575ba3e398
anshulruhil02 <anshulruhil02@gmail.com>
2024-01-19 04:39:05 -0500

    Initial Commit

---
+++ FraternityManagement/FraternityManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  FraternityManagementApp.swift
+//  FraternityManagement
+//
+//  Created by Anshul Ruhil on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct FraternityManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
