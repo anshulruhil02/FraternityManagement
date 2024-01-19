39c1b76f48f2a0382a9270afcd428eb3d957636a
anshulruhil02 <anshulruhil02@gmail.com>
2024-01-19 04:49:16 -0500

    First commit oooo

--- FraternityManagement/ContentView.swift
+++ FraternityManagement/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Sigma Chi")
+            Text("Sigma Chi yo")
         }
         .padding()
     }
